3cbe312bdbda6e5997b6eba5266933e20803cd99
sparks <sparks@doesnotexist.com>
2013-05-11 23:51:27 +0100

    Updated comment for main function

--- HelloWorld/src/sparks/helloworld/HelloWorld.java
+++ HelloWorld/src/sparks/helloworld/HelloWorld.java
@@ -2,6 +2,7 @@ package sparks.helloworld;
 
 public class HelloWorld {
 	public static void main(String[] args) {
+		// Line to print out hello message
 		System.out.println("Hello World and " + args[0] + "!!!!");
 	}
 }
