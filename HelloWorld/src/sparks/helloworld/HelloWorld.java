2a421e20920dcaa7da09e8ccb0da455d06ec9544
sparks <sparks@doesnotexist.com>
2013-05-11 23:06:51 +0100

    Added parameter to hello world statement
    Signed-off-by: sparks <sparks@doesnotexist.com>

--- HelloWorld/src/sparks/helloworld/HelloWorld.java
+++ HelloWorld/src/sparks/helloworld/HelloWorld.java
@@ -2,6 +2,6 @@ package sparks.helloworld;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World!!!!");
+		System.out.println("Hello World and " + args[0] + "!!!!");
 	}
 }
